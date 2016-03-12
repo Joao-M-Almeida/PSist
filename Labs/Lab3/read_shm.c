3c7d977c378d27d2a66527bab0d7938f4fad9adf
Joao-M-Almeida <jmirandadealme@gmail.com>
2016-03-12 18:34:21 +0000

    Lab3 ex4, unfinished

--- Labs/Lab3/read_shm.c
+++ Labs/Lab3/read_shm.c
@@ -1,5 +1,5 @@
 /*
-gcc -Wall -pedantic -ansi -std=gnu99 ex4.c -o ex4_gen_random.out -lrt
+gcc -Wall -pedantic -ansi -std=gnu99 read_shm.c -o ex4_read_shm.out -lrt
 */
 
 #include <limits.h>
@@ -11,21 +11,13 @@ gcc -Wall -pedantic -ansi -std=gnu99 ex4.c -o ex4_gen_random.out -lrt
 #include <sys/mman.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <time.h>
-#include <signal.h>
 
 
-int fd;
-int * shared;
-unsigned int shared_mem_size ;
 int count_even = 0;
 int count_odd = 0;
-
-void error_and_die(char error[]) {
-    printf("%s\n", error );
-    exit(-1);
-}
-
+unsigned int shared_mem_size = sizeof(int);
+int fd;
+int * shared;
 
 void alarm_handler(int signum){
     printf("Received signal: %d\n", signum);
@@ -35,39 +27,42 @@ void alarm_handler(int signum){
     close(fd);
     munmap(shared, shared_mem_size);
     exit(0);
-    
+
 }
 
-
 int main() {
-
-    int shared_mem_size = sizeof(int);
-    srandom(getpid());
-
-
-    fd = shm_open("/rand_nums", O_CREAT | O_TRUNC | O_RDWR, 0666);
-    if (fd == -1)
-        error_and_die("shm_open");
-    int r = ftruncate(fd, shared_mem_size);
-    if (r != 0)
-        error_and_die("ftruncate");
+    fd = shm_open("/rand_nums", O_CREAT | O_RDWR, 0666);
+    if (fd == -1){
+        printf("Unable to open\n");
+        exit(-1);
+    }
     shared = (int *) mmap(NULL, shared_mem_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+
+    int read;
+    int last_read=-1;
 
     struct sigaction action;
     action.sa_handler = alarm_handler;
     sigaction(SIGALRM, &action, NULL);
-    alarm(10);
+    alarm(20);
 
-    int num;
 
 
     while(1){
-        num = random();
-        (*shared) = num;
-        if (num%2==0)
-            count_even++;
-        else
-            count_odd++;
+        read = (*shared);
+        if(read!=last_read){
+            if(read%2==0)
+                count_even++;
+            else
+                count_odd++;
+            last_read = read;
+        }
+
 
     }
+
+
+    close(fd);
+    munmap(shared, shared_mem_size);
+    exit(0);
 }
