34fae0d725eb3aa16f0d7b975a4364bfae135916
Joao-M-Almeida <jmirandadealme@gmail.com>
2016-04-04 00:12:00 +0100

    Lab5 version2

---
+++ Labs/Lab5/server2nd.c
@@ -0,0 +1,85 @@
+/*    gcc -Wall -pedantic -ansi -std=gnu99 server-skel.c -o server1.out    */
+
+#include "storyserver.h"
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <signal.h>
+
+int sock;
+char * story;
+
+void clean_up(int exit_val){
+    printf("Cleaning UP... \n");
+    free(story);
+    close(sock);
+    unlink(MY_SOCK_PATH);
+    exit(exit_val);
+}
+
+void exit_gracefuly(int signum){
+    printf("Received signal: %d\n", signum);
+    clean_up(0);
+}
+
+int main(){
+    story = strdup("");
+    char * read_msg;
+     /* create socket  */
+
+
+    struct sigaction action;
+    action.sa_handler = exit_gracefuly;
+    sigaction(SIGINT, &action, NULL);
+
+    if((sock = socket(AF_UNIX, SOCK_DGRAM,0)) == -1){
+        perror("Socket Error");
+        clean_up(-1);
+    }
+
+    struct sockaddr_un addr;
+    memset(&addr, 0, sizeof(struct sockaddr_un)); /* Clear structure */
+    addr.sun_family = AF_UNIX;
+    strncpy(addr.sun_path, MY_SOCK_PATH, sizeof(addr.sun_path) - 1);
+
+    unsigned int len_addr = sizeof(addr);
+
+    if(bind(sock, (const struct sockaddr *) &addr,len_addr)==-1){
+        perror("Socket Error");
+        clean_up(-1);
+    }
+
+    int len_rcvd;
+
+    struct sockaddr_un sender_addr;
+
+    unsigned int len_sender_addr = sizeof(sender_addr);
+
+    while(1){
+        /* read message */
+
+        len_rcvd = recvfromwrapped(&read_msg, (struct sockaddr *) &sender_addr, &len_sender_addr, sock);
+        if(len_rcvd == -1){
+            perror("Read Error");
+            clean_up(-1);
+        }
+
+        printf("Received %s\n in %d bytes", read_msg, len_rcvd);
+        /* process message */
+
+        story = (char *) realloc(story, strlen(story) + strlen(read_msg) +1);
+        strcat(story, read_msg);
+        printf("Story: %s\n", story);
+    }
+    printf("OK\n");
+    clean_up(0);
+}
--- Labs/Lab5/storyserver.h
+++ Labs/Lab5/storyserver.h
@@ -1,10 +1,74 @@
 
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include "stdio.h"
+#include "stdlib.h"
+
 #define MESSAGE_LEN 100
 
 typedef struct message{
     char buffer[MESSAGE_LEN];
+    int last;
 } message;
 
 #define MY_SOCK_PATH "somepath"
 
 #define CLIENT_SOCK  "client_N"
+
+/*
+
+Using the struct message sends a message of abitrary lenght _len_ to _to_addr_ through the DGRAM socket sock
+Returns -1 on failure and the total number of bytes sent on success.
+
+*/
+
+int sendtowrapped(char * msg, int len,   const struct sockaddr * to_addr, int len_addr, int sock ){
+    printf("Sending message: %s of %d chars\n",msg,len);
+    if(len < MESSAGE_LEN){
+        message m;
+        strncpy(m.buffer,msg,len);
+        m.last=1;
+        return sendto(sock, &m, sizeof(m), 0,  (const struct sockaddr *) to_addr, len_addr);
+    }else{
+        char * ptr = msg;
+        int len_to_send;
+        message m;
+        m.last = 0;
+        int sent_bytes = 0;
+        do{
+            if(strlen(ptr)<MESSAGE_LEN){
+                m.last = 1;
+                len_to_send = strlen(ptr)+1;
+            } else {
+                m.last = 0;
+                len_to_send =  MESSAGE_LEN;
+            }
+            strncpy(m.buffer,ptr,len_to_send);
+            sent_bytes += sizeof(m);
+            if(sendto(sock, &m, sizeof(m), 0,  (const struct sockaddr *) to_addr, len_addr)==-1){
+                return -1;
+            }
+            ptr = ptr + len_to_send;
+        }while(m.last!=1);
+        return sent_bytes;
+    }
+    return -1;
+}
+
+int recvfromwrapped(char ** msg, struct sockaddr * sender_addr, unsigned int * len_sender_addr, int sock){
+    message m;
+    int total_recvd = sizeof(m);
+    (*msg) = (char * ) malloc(sizeof(char));
+    (**msg) = '\0';
+    do{
+        if(recvfrom(sock, &m ,sizeof(m) , 0, (struct sockaddr *) sender_addr, len_sender_addr) ==-1){
+            return -1;
+        }
+        (*msg) = (char * ) realloc((*msg), strlen(*msg) + strlen(m.buffer) +1);
+        strcat((*msg), m.buffer);
+        total_recvd += sizeof(m);
+    }while(m.last == 0);
+    return total_recvd;
+}
