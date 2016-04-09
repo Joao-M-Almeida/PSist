7f1936b7ba52f72ac3a0d0159d6a9709f377b98c
André Stielau <a.m.stielau@gmail.com>
2016-04-09 18:12:44 +0100

    hash functions

---
+++ Labs/Lab6/hash-lib.h
@@ -0,0 +1,20 @@
+#include <stdbool.h>
+#include <stdint.h>
+
+typedef void* Item;
+typedef void (*delete_func) (Item);
+
+typedef struct _hash_item {
+    struct _hash_item *next;
+    uint32_t key;
+    Item item;
+} hash_item;
+
+typedef hash_item** hash_table;
+
+hash_table create_hash(uint32_t size);
+void delete_hash(hash_table hash, uint32_t size, void (*delete_func) (Item));
+uint32_t hash_function(uint32_t key, uint32_t size);
+Item read_item(hash_table hash, uint32_t key, uint32_t size);
+bool insert_item(hash_table hash, Item item, uint32_t key, uint32_t size);
+bool delete_item(hash_table hash, uint32_t key, uint32_t size, void (*delete_func) (Item));
